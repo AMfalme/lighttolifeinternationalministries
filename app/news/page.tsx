8b5d2d9ea34b9eefba3bef9b4714fda31c407db5
AMfalme <mfalmegriffin@gmail.com>
2026-06-11 09:49:38 +0300

    fix suspense error for news file.

--- app/news/page.tsx
+++ app/news/page.tsx
@@ -1,5 +1,10 @@
+import { Suspense } from "react";
 import NewsClient from "./NewsClient";
 
 export default function NewsPage() {
-  return <NewsClient />;
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <NewsClient />
+    </Suspense>
+  );
 }
